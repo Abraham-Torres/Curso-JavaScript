3d9c6cffe574fdfebcb800b6346bf4c337639c55
Abraham-Torres <abraham_torres81@outlook.com>
2022-10-18 17:56:52 -0500

    Descuentos queda pendiente

---
+++ Controles-Flujo/02-Ejercicios-Control_Flujo/02-Descuentos.js
@@ -0,0 +1,9 @@
+var valorCompra=10;
+
+if (valorCompra<=10){
+    valorCompra+=3
+    console.log(valorCompra);
+}else if (valorCompra>10 && valorCompra<=20){
+    valorCompra+=5
+    console.log(valorCompra);
+}
