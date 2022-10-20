d7a69bba894caae784a52e117164ea6b6308a74e
Abraham-Torres <abraham_torres81@outlook.com>
2022-10-20 14:49:58 -0500

    ejercicio 2 INCOMPLETO

--- Controles-Flujo/02-Ejercicios-Control_Flujo/02-Descuentos.js
+++ Controles-Flujo/02-Ejercicios-Control_Flujo/02-Descuentos.js
@@ -1,9 +1,34 @@
-var valorCompra=10;
+//Manera con if
+var compra=51;
 
-if (valorCompra<=10){
-    valorCompra+=3
-    console.log(valorCompra);
-}else if (valorCompra>10 && valorCompra<=20){
-    valorCompra+=5
-    console.log(valorCompra);
+if(compra<=10){
+    compra+=3;
+    console.log(`El total de la compra con envio incluido es de ${compra}`);
+}else if(compra>10 && compra<=20){
+    compra+=5;
+    console.log(`El total del la compra con envio incluido es de ${compra}`);
+}else if(compra>20 && compra<=50){
+    compra+=7;
+    console.log(`El total de la compra con envio incluido es de ${compra}`);
+}else if(compra>50){
+    console.log(`Envio GRATIS total a pagar: ${compra}`);
+}else{
+    console.log("ha ocurrido algun error");
+}
+
+
+var compra=10;
+
+switch(compra){
+    case compra<=10:
+        compra+=3;
+        console.log(compra);
+        break;
+}
+
+switch(compra){
+    case compra >10 && compra<=20:
+        compra+=5;
+        console.log(compra);
+        break;
 }
